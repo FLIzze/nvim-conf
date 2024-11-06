cd7717eeff9c6ba0b70181c86e1367bbc545c4ac
flizze <alexandre.bel@hotmail.com>
2024-11-06 14:20:35 +0100

    added undo tree, tagalong and fugitive

--- lua/flizze/remap.lua
+++ lua/flizze/remap.lua
@@ -12,3 +12,10 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
 vim.keymap.set("n", "<leader>y", "\"+y")
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+
+vim.keymap.set("n", "<C-q>", "<C-w>q")
---
+++ lua/plugins/tailwindcss-colors.lua
@@ -0,0 +1,7 @@
+return {
+    'themaxmarchuk/tailwindcss-colors.nvim',
+    config = function()
+        require('tailwindcss-colors').setup({})
+    end,
+}
+
